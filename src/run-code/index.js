3a7ab7fb2fb61ec3ae57dc947293266034d1a8d8
Deepak <deepaknallam3@gmail.com>
2025-06-07 12:04:41 +0530

    refactor(log-err): refactor to code execution so that it logs errors for each testcase

--- src/run-code/index.js
+++ src/run-code/index.js
@@ -6,85 +6,84 @@ const {info} = require("./info")
 const {spawn} = require("child_process");
 
 async function runCode({language = "", code = "", input = ""}) {
-    const timeout = 30;
+  const timeout = 30;
 
-    if (code === "")
-        throw {
-            status: 400,
+  if (code === "")
+    throw {
+      status: 400,
             error: "No Code found to execute."
         }
 
-    if (!supportedLanguages.includes(language))
-        throw {
-            status: 400,
+  if (!supportedLanguages.includes(language))
+    throw {
+      status: 400,
             error: `Please enter a valid language. The languages currently supported are: ${supportedLanguages.join(', ')}.`
         }
 
     const {jobID} = await createCodeFile(language, code);
     const {compileCodeCommand, compilationArgs, executeCodeCommand, executionArgs, outputExt} = commandMap(jobID, language);
 
-    if (compileCodeCommand) {
-        await new Promise((resolve, reject) => {
+  if (compileCodeCommand) {
+    await new Promise((resolve, reject) => {
             const compileCode = spawn(compileCodeCommand, compilationArgs || [])
             compileCode.stderr.on('data', (error) => {
-                reject({
-                    status: 200,
+        reject({
+          status: 200,
                     output: '',
-                    error: error.toString(),
+          error: error.toString(),
                     language
                 })
-            });
+      });
             compileCode.on('exit', () => {
                 resolve()
             })
         })
-    }
+  }
 
-    const result = await new Promise((resolve, reject) => {
-        const executeCode = spawn(executeCodeCommand, executionArgs || []);
+  const result = await new Promise((resolve, reject) => {
+    const executeCode = spawn(executeCodeCommand, executionArgs || []);
         let output = "", error = "";
 
-        const timer = setTimeout(async () => {
-            executeCode.kill("SIGHUP");
+    const timer = setTimeout(async () => {
+      executeCode.kill("SIGHUP");
 
-            await removeCodeFile(jobID, language, outputExt);
+      await removeCodeFile(jobID, language, outputExt);
 
-            reject({
-                status: 408,
-                error: `DeepCode API Timed Out. Your code took too long to execute, over ${timeout} seconds. Make sure you are sending input as payload if your code expects an input.`
-            })
-        }, timeout * 1000);
+      reject({
+        status: 408,
+        error: `DeepCode API Timed Out. Your code took too long to execute, over ${timeout} seconds.`,
+        language,
+      });
+    }, timeout * 1000);
 
-        if (input !== "") {
-            input.split('\n').forEach((line) => {
-                executeCode.stdin.write(`${line}\n`);
-            });
-            executeCode.stdin.end();
-        }
+    if (input !== "") {
+      executeCode.stdin.write(input);
+      executeCode.stdin.end();
+    }
 
-        executeCode.stdin.on('error', (err) => {
-            console.log('stdin err', err);
-        });
-
-        executeCode.stdout.on('data', (data) => {
-            output += data.toString();
-        });
+    executeCode.stdout.on("data", (data) => {
+      output += data.toString();
+    });
 
         executeCode.stderr.on('data', (data) => {
-            error += data.toString();
-        });
+      error += data.toString();
+    });
 
-        executeCode.on('exit', (err) => {
-            clearTimeout(timer);
-            resolve({output, error});
-        });
-    })
+    executeCode.on("exit", (code) => {
+      clearTimeout(timer);
+      resolve({
+        output: output.trim(),
+        error: error.trim(),
+        exitCode: code,
+      });
+    });
+  });
 
-    await removeCodeFile(jobID, language, outputExt);
+  await removeCodeFile(jobID, language, outputExt);
 
-    return {
-        ...result,
-        language,
+  return {
+    ...result,
+    language,
         info: await info(language)
     }
 }
