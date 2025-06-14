9230dd5ac0008bc0a9845bcf0ea951305b143f46
Deepak <deepaknallam3@gmail.com>
2025-06-14 11:56:26 +0530

    add(healthcheck): add healthcheck endpoint to DeepCode api

--- src/index.js
+++ src/index.js
@@ -1,49 +1,52 @@
-const {runCode} = require("./run-code");
-const {supportedLanguages} = require("./run-code/instructions");
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const {runCode} = require("./run-code");
+const {supportedLanguages} = require("./run-code/instructions");
+const {info} = require("./run-code/info");
+
 const app = express();
 const port = process.env.PORT || 8080;
-const cors = require("cors");
-const {info} = require("./run-code/info");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 const sendResponse = (res, statusCode, body) => {
-    const timeStamp = Date.now()
-
+    const timeStamp = Date.now();
     res.status(statusCode).send({
         timeStamp,
         status: statusCode,
         ...body
-    })
-}
+    });
+};
 
 app.post("/", async (req, res) => {
     try {
-        const output = await runCode(req.body)
-        sendResponse(res, 200, output)
+        const output = await runCode(req.body);
+        sendResponse(res, 200, output);
     } catch (err) {
-        sendResponse(res, err?.status || 500, err)
+        sendResponse(res, err?.status || 500, err);
     }
-})
+});
 
-app.get('/list', async (req, res) => {
-    const body = []
+app.get("/health-check", (req, res) => {
+    res.status(200).send("OK");
+});
 
-    for(const language of supportedLanguages) {
+app.get("/list", async (req, res) => {
+    const body = [];
+
+    for (const language of supportedLanguages) {
         body.push({
             language,
             info: await info(language),
-        })
+        });
     }
 
-    sendResponse(res, 200, {supportedLanguages: body})
-})
+    sendResponse(res, 200, {supportedLanguages: body});
+});
 
-app.listen(port);
-
-console.log('Starting server on port', port);
+app.listen(port, () => {
+    console.log("Starting server on port", port);
+});
